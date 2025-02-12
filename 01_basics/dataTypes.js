a687dce9b1c9454709d4578776154004801323a6
Ani811 <royani811611@gmail.com>
2025-02-12 21:06:00 +0000

    container config done

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,9 @@
+let age=10; //number
+let name="Animesh"  //string
+let isHappy=true    //boolean
+let future;     //undeine
+let money=null  //null->null value not undefine
+
+//symbol    =>uniqueless 
+
+//object    => discuss later

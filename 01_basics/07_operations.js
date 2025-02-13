6e2364c5cc020b93ebc6392c1039888ba68e2315
Ani811 <royani811611@gmail.com>
2025-02-13 09:37:43 +0000

    change location

---
+++ 01_basics/07_operations.js
@@ -0,0 +1,23 @@
+let value=3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  //2 power 2
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="Animesh"
+let str2="Bhumika"
+
+let str3= str1 + str2;
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 +2 + "2");
+
+
